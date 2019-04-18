a853a4dd5a99d3cdf9185481d56a3a19a9706f79
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-04-18 10:56:10 -0300

    Refactor element by element operations

--- src/mod_matrix/mod_matrix.c
+++ src/mod_matrix/mod_matrix.c
@@ -3,52 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-matrix_t*
-mod_matrix_sum(matrix_t* m1, matrix_t* m2)
+/**
+ * @brief Applies op between elements of the
+ * matrices: m1[i,j] op m2[i,j] ; 0<i<rows, 0<j<cols.
+ * Matrices sizes must match!
+ *
+ * @param m1 Matrix 1
+ * @param m2 Matrix 2
+ * @param op Operator
+ * @return matrix_t*
+ */
+static inline matrix_t*
+mod_matrix_binary_elem_op_applier(matrix_t* m1,
+                                  matrix_t* m2,
+                                  uint32_t (*op)(uint32_t, uint32_t))
 {
   if (m1->cols != m2->cols || m1->rows != m2->rows) {
     fprintf(stderr, "Matrix sizes must match!");
     exit(EXIT_FAILURE);
   }
 
-  matrix_t* sum = NEW_MATRIX(m1->cols, m1->rows);
-  sum->rows     = m1->rows;
-  sum->cols     = m1->cols;
+  matrix_t* ans = NEW_MATRIX(m1->cols, m1->rows);
+  ans->rows     = m1->rows;
+  ans->cols     = m1->cols;
 
-  uint32_t* sum_p = sum->values;
+  uint32_t* ans_p = ans->values;
   uint32_t* m1_p  = m1->values;
   uint32_t* m2_p  = m2->values;
-  uint32_t i      = sum->cols * sum->rows;
+  uint32_t i      = ans->cols * ans->rows;
 
   while (i--) {
-    *(sum_p++) = mod_sum(*(m1_p++), *(m2_p++));
+    *(ans_p++) = op(*(m1_p++), *(m2_p++));
   }
 
-  return sum;
+  return ans;
+}
+
+matrix_t*
+mod_matrix_sum(matrix_t* m1, matrix_t* m2)
+{
+  return mod_matrix_binary_elem_op_applier(m1, m2, mod_sum);
 }
 
 matrix_t*
 mod_matrix_sub(matrix_t* m1, matrix_t* m2)
 {
-  if (m1->cols != m2->cols || m1->rows != m2->rows) {
-    fprintf(stderr, "Matrix sizes must match!");
-    exit(EXIT_FAILURE);
-  }
-
-  matrix_t* sub = NEW_MATRIX(m1->cols, m1->rows);
-  sub->rows     = m1->rows;
-  sub->cols     = m1->cols;
-
-  uint32_t* sub_p = sub->values;
-  uint32_t* m1_p  = m1->values;
-  uint32_t* m2_p  = m2->values;
-  uint32_t i      = sub->cols * sub->rows;
-
-  while (i--) {
-    *(sub_p++) = mod_sub(*(m1_p++), *(m2_p++));
-  }
-
-  return sub;
+  return mod_matrix_binary_elem_op_applier(m1, m2, mod_sub);
 }
 
 matrix_t*
