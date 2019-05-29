eb0e237c1bae711496ce1637898f5ac443f776f8
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-05-28 21:34:52 -0300

    Perform small refactors

--- src/include/mod_matrix/mod_matrix_macros.h
+++ src/include/mod_matrix/mod_matrix_macros.h
@@ -1,5 +1,5 @@
-#ifndef MOD_MATRIX_MACROS_H_
-#define MOD_MATRIX_MACROS_H_
+#ifndef MOD_MATRIX_MACROS_H_23d9ea1f6208868426a402e806ced9f3
+#define MOD_MATRIX_MACROS_H_23d9ea1f6208868426a402e806ced9f3
 
 #include <mod_matrix/mod_matrix.h>
 #include <stdlib.h>
@@ -7,4 +7,4 @@
 #define NEW_MATRIX(rows, cols)                                                 \
   (malloc(sizeof(matrix_t) + (rows) * (cols) * sizeof(uint32_t)))
 
-#endif
+#endif // MOD_MATRIX_MACROS_H_23d9ea1f6208868426a402e806ced9f3
